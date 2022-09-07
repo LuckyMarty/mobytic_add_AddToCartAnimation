7a983547552a3b2f2fb67dc151f4b5df2a827d5b
Lucky Marty <l.marty@mobytic.com>
2022-09-07 11:04:19 +0200

    First commit & final

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Mobytic - Addons - Cart Animation
---
+++ mobytic_add_AddToCartAnimation.php
@@ -0,0 +1,275 @@
+<?php
+
+/**
+ * 2007-2022 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2022 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Mobytic_add_AddToCartAnimation extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'mobytic_add_AddToCartAnimation';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'Mobytic';
+        $this->need_instance = 1;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Mobytic - Addons - Cart Animation');
+        $this->description = $this->l('Add add to cart animation');
+
+        $this->confirmUninstall = $this->l('');
+
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        $this->installTab();
+
+        Configuration::updateValue('MOBYTIC_ADD_ADDTOCARTANIMATION_LIVE_MODE', false);
+
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader') &&
+            $this->registerHook('displayHeader');
+    }
+
+    public function installTab()
+    {
+        $response = true;
+
+        // First check for parent tab
+        $parentTabID = Tab::getIdFromClassName('AdminMobytic');
+
+        if ($parentTabID) {
+            $parentTab = new Tab($parentTabID);
+        } else {
+            $parentTab = new Tab();
+            $parentTab->active = 1;
+            $parentTab->name = array();
+            $parentTab->class_name = "AdminMobytic";
+            foreach (Language::getLanguages() as $lang) {
+                $parentTab->name[$lang['id_lang']] = "Mobytic";
+            }
+            $parentTab->id_parent = 0;
+            $parentTab->module = $this->name;
+            $response &= $parentTab->add();
+        }
+
+        // Check for parent tab2
+        $parentTab_2ID = Tab::getIdFromClassName('AdminMobyticAddons');
+        if ($parentTab_2ID) {
+            $parentTab_2 = new Tab($parentTab_2ID);
+        } else {
+            $parentTab_2 = new Tab();
+            $parentTab_2->active = 1;
+            $parentTab_2->name = array();
+            $parentTab_2->class_name = "AdminMobyticAddons";
+            foreach (Language::getLanguages() as $lang) {
+                $parentTab_2->name[$lang['id_lang']] = "Addons";
+            }
+            $parentTab_2->id_parent = $parentTab->id;
+            $parentTab_2->module = $this->name;
+            $response &= $parentTab_2->add();
+        }
+
+        // Created tab
+        $tab = new Tab();
+        $tab->active = 1;
+        $tab->class_name = 'Admin' . $this->name;
+        $tab->name = array();
+        foreach (Language::getLanguages() as $lang) {
+            $tab->name[$lang['id_lang']] = "Animation Panier";
+        }
+        $tab->id_parent = $parentTab_2->id;
+        $tab->module = $this->name;
+        $response &= $tab->add();
+
+        return $response;
+    }
+
+    public function uninstall()
+    {
+        $this->uninstallTab();
+
+        Configuration::deleteByName('MOBYTIC_ADD_ADDTOCARTANIMATION_LIVE_MODE');
+
+        return parent::uninstall();
+    }
+
+    public function uninstallTab()
+    {
+        $id_tab = Tab::getIdFromClassName('Admin' . $this->name);
+        $tab = new Tab($id_tab);
+        $tab->delete();
+        return true;
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool)Tools::isSubmit('submitMobytic_add_AddToCartAnimationModule')) == true) {
+            $this->postProcess();
+        }
+
+        $this->context->smarty->assign('module_dir', $this->_path);
+
+        $output = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/configure.tpl');
+
+        return $output . $this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitMobytic_add_AddToCartAnimationModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->l('Settings'),
+                    'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Live mode'),
+                        'name' => 'MOBYTIC_ADD_ADDTOCARTANIMATION_LIVE_MODE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Use this module in live mode'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled')
+                            )
+                        ),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        return array(
+            'MOBYTIC_ADD_ADDTOCARTANIMATION_LIVE_MODE' => Configuration::get('MOBYTIC_ADD_ADDTOCARTANIMATION_LIVE_MODE', true),
+            'MOBYTIC_ADD_ADDTOCARTANIMATION_ACCOUNT_EMAIL' => Configuration::get('MOBYTIC_ADD_ADDTOCARTANIMATION_ACCOUNT_EMAIL', 'contact@prestashop.com'),
+            'MOBYTIC_ADD_ADDTOCARTANIMATION_ACCOUNT_PASSWORD' => Configuration::get('MOBYTIC_ADD_ADDTOCARTANIMATION_ACCOUNT_PASSWORD', null),
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        $form_values = $this->getConfigFormValues();
+
+        foreach (array_keys($form_values) as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be loaded in the BO.
+     */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path . 'views/js/back.js');
+            $this->context->controller->addCSS($this->_path . 'views/css/back.css');
+        }
+    }
+    
+    public function hookDisplayHeader()
+    {
+        if (Configuration::get('MOBYTIC_ADD_ADDTOCARTANIMATION_LIVE_MODE') == true) {
+            $this->context->controller->addJS($this->_path . 'views/js/front.js');
+            $this->context->controller->addCSS($this->_path . '/views/css/front.css');
+        }
+        /* Place your code here. */
+    }
+}
